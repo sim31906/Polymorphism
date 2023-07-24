b84bc3c9470b77bf616bade996b27f4f265d0b4c
Jakaphun Jarujitranon <jakaphun_jaruji@cmu.ac.th>
2023-07-24 23:41:20 +0700

    finish

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         Cow cow = new Cow();
         Duck donald = new Duck();
         Owl owl = new Owl();
